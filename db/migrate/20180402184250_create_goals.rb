8bc1817e73130546bee640e28a659972cc44232f
Jason Nedell <jason@pnc.one>
2018-04-02 12:43:15 -0600

    Generate Goal model

---
+++ db/migrate/20180402184250_create_goals.rb
@@ -0,0 +1,15 @@
+class CreateGoals < ActiveRecord::Migration[5.1]
+  def change
+    create_table :goals do |t|
+      t.references :user, foreign_key: true
+      t.text :name
+      t.text :description
+      t.decimal :amount
+      t.datetime :posted
+      t.boolean :public
+      t.boolean :deleted
+
+      t.timestamps
+    end
+  end
+end
