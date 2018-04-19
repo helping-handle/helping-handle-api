4b95da0ec3ee603427a7b127c56f6df640ad90be
Jason Nedell <jason@pnc.one>
2018-04-19 00:43:17 -0600

    Add devise_jwt
    Add auth headers to rack_cors
    Add jwt blacklist model and migrations

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
     resource '*',
-      headers: :any,
+      headers: %w(Authorization),
+      expose: %w(Authorization),
+      max_age: 600,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
