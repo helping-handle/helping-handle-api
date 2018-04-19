6821be36dc3d16c2f153d6b51b1e4e79c6cf11ba
Jason Nedell <jason@pnc.one>
2018-04-19 03:31:25 -0600

    Don’t give default email or password

--- db/migrate/20180416031529_devise_create_users.rb
+++ db/migrate/20180416031529_devise_create_users.rb
@@ -12,8 +12,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.text    :handle_paypal
 
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
