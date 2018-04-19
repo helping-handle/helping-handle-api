705840b1051b344cb62124d90ad53bf83a3dc951
Jason Nedell <jason@pnc.one>
2018-04-19 15:54:19 -0600

    Add User actions
    Use symbolic names for status codes
    Add show action
    Add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,39 @@ class UsersController < ApplicationController
 
   def index
     if user_signed_in? && current_user.admin?
-      render json: Goal.includes(:user).all
+      render json: User.all
     else
-      render json: { msg: 'Action forbidden' }, status: 403
+      render json: { msg: 'Action forbidden' }, status: :forbidden
     end
   end
 
+  def show
+    if user_signed_in? && current_user.admin?
+      render json: @user
+    else
+      render json: @user, :only => [:id, :handle]
+    end
+  end
 
   def update
+    if !user_signed_in? || @user != current_user
+      render json: { msg: 'Action forbidden' }, status: :forbidden
+    elsif @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name, :description, :email, :handle_cash, :handle_venmo,
+      :handle_paypal)
   end
 end
