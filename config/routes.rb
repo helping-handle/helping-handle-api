30c1df751e7ec1c638769f46f46e59bb378fe7c2
Jason Nedell <jason@pnc.one>
2018-04-19 07:38:27 -0600

    Succesfully return token

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :favorites
     resources :categories
     resources :goals
-    devise_for :users
+    devise_for :users, controllers: {sessions: 'sessions'}
+    devise_scope :user do
+        get 'users/current', to: 'sessions#show'
+    end
   end
 end
