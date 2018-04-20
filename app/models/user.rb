5ae85b627aca0f229775f526bcc685561f4b5850
zinefer <zinefer@gmail.com>
2018-04-19 21:44:24 -0600

    add things

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  has_many :donations
   enum role: [ :donor, :recipient, :admin ]
 end
