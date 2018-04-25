a143d3bfd07ff8c4753f48141139b73d37112548
zinefer <zinefer@gmail.com>
2018-04-24 23:10:45 -0600

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
   defaults format: :json do
-    resources :resources
-    resources :donations, only: [:index, :show, :update, :destroy]
-    resources :categories
-
-    resources :users, only: [:index, :show, :update] do
-      resources :goals, only: [:index]
-    end
-    resources :goals, only: [:show, :edit, :create, :update, :destroy]
+    resources :goals,       only: [:index, :show]
+    resources :resources,   only: [:index, :show]
+    resources :donations,   only: [:create]
+    resources :users,       only: [:index, :show]
 
     authenticate :user do
+      resources :goals,     only: [:create, :update, :destroy]
+      resources :resources, only: [:create, :update, :destroy]
+      resources :donations, only: [:update, :show]
       resources :favorites
+      resources :users, only: [:update] do
+        resources :goals, only: [:index]
+      end
+      patch 'goals/:id/toggle_favorite', to: 'goals#toggle_favorite'
     end
-
-    patch 'goals/:id/toggle_favorite', to: 'goals#toggle_favorite'
 
     devise_for :users, path: 'auth/', controllers: {sessions: 'sessions', registrations: 'registrations' }
     devise_scope :user do
